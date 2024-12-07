da8c25914508ad8ad2ee19cb80aee50c98b2c3ea
ALLEN PARK <pmh621@naver.com>
2024-12-07 15:29:05 +0900

    Create all_stock_data_downloader_chunk.py
    Download all stock data with Chunk method
    주식데이터 다운(chunk를 이용해 속도 높이기)
    使用大块法(chunk)下载股票数据

---
+++ all_stock_data_downloader_chunk.py
@@ -0,0 +1,48 @@
+
+import yfinance as yf
+import pandas as pd
+from tqdm import tqdm
+
+# target_DB CSV file can be downloaded from the "tickers DB" folder on my GitHub repository
+# this step drives all tickers lists from the CSV file. if you have all tickers list file, you can skip this step and use your own ticker list
+
+target_DB = "KRX_DB" 
+parquet_file_path = f"./{target_DB}.parquet"
+
+################################################################################################
+################################################################################################
+
+
+# initial steps
+
+file = pd.read_csv("./" + target_DB + ".csv") # laod target_DB csv file from your own directory path
+tickers = file["tickers"].tolist() 
+
+# Downloading stock data using yfinance is too slow, so I used the " chunk " method. 
+
+chunk_size = 100 #chunk_size can be adjusted. 
+data_frames = []
+
+for i in tqdm(range(0, len(tickers), chunk_size), desc="Downloading chunks"):
+    chunk = tickers[i:i + chunk_size]
+    print(f"Downloading chunk: {chunk}")
+    try:
+        # 티커 청크 다운로드
+        data = yf.download(chunk, period="6mo", group_by="ticker", progress=False)
+        data_frames.append(data)
+    except Exception as e:
+        print(f"Error downloading {chunk}: {e}")
+    
+    if i == len(tickers):
+        print("다운로드 완료!")
+
+# Integrate data frame 
+final_df = pd.concat(data_frames, axis=1)
+
+
+# save parquet file on yor own path.
+final_df.to_parquet(parquet_file_path)
+
+
+# load parquet data
+parquet_df = pd.read_parquet(parquet_file_path)
